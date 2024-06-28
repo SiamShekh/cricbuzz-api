b21ab980c0737485c338524fd7805c7c8daf0bf3
Md. Siam Shekh <siam62349@gmail.com>
2024-06-28 16:01:28 +0600

    Live match added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const upcoming_match_controller = require('./src/upcoming-match-controller');
 const recent_match_controller = require('./src/recent-match-controller');
+const live_match_controller = require('./src/live-match-controller');
 const app = express();
 
 
@@ -11,6 +12,7 @@ app.use(cors());
 
 app.get('/api/v1/match/upcoming', upcoming_match_controller);
 app.get('/api/v1/match/recent', recent_match_controller);
+app.get('/api/v1/match/live', live_match_controller);
 
 app.listen(3000, () => {
     console.log(`SERVER RUNNING AT 3000`);
