7e03c4b07eba58167d1aac602e1e21af495d8b9c
Md. Siam Shekh <siam62349@gmail.com>
2024-06-30 06:44:32 +0600

    Inning 1 & 2 Data Added on Api

--- src/info/scorecards.js
+++ src/info/scorecards.js
@@ -43,6 +43,7 @@ const scorecards = async (req, res) => {
     //div#innings_1 > div.cb-ltst-wgt-hdr
     const innings1_batting = [];
     const innings1_fall_of_wickets = [];
+    const innings1_wickets = [];
 
     $(innings1).children().each(function (e, ele) {
         if (e === 0) {
@@ -90,6 +91,129 @@ const scorecards = async (req, res) => {
                 const bowl = $(this).text().split(",")[1].split(")")[0].trim();
                 innings1_fall_of_wickets.push({ player, player_id, runs, bowl })
             })
+        } else if (e === 3) {
+            const Bowler = $(this);
+            $(Bowler).children().each(function () {
+                const BowlerItem = $(this);
+                $(BowlerItem).each(function () {
+                    const Name = $(this).find("a.cb-text-link").text();
+
+                    const playerNameHref = $(this).find("a.cb-text-link").attr('href') || '/profiles/1427578/zakir-patel';
+                    const PlayerIdPart = playerNameHref.split("profiles/")[1];
+                    const PlayerId = PlayerIdPart.split("/")[0];
+
+                    const Over = $(this).find("div.cb-col.cb-col-8.text-right").eq(0).text();
+                    const Maden = $(this).find("div.cb-col.cb-col-8.text-right").eq(1).text();
+                    const Run = $(this).find("div.cb-col.cb-col-10.text-right").eq(0).text();
+                    const Wickets = $(this).find("div.cb-col.cb-col-8.text-right").eq(2).text();
+                    const NoBowl = $(this).find("div.cb-col.cb-col-8.text-right").eq(3).text();
+                    const Wide = $(this).find("div.cb-col.cb-col-8.text-right").eq(4).text();
+                    const Eco = $(this).find("div.cb-col.cb-col-10.text-right").eq(1).text();
+
+                    if (Name) {
+                        innings1_wickets.push({
+                            Name,
+                            PlayerId,
+                            Over,
+                            Maden,
+                            Run,
+                            Wickets,
+                            NoBowl,
+                            Wide,
+                            Eco
+                        })
+                    }
+                })
+            })
+        }
+    });
+
+    const innings2 = $(infos).find('div > div#innings_2');
+    //div#innings_1 > div.cb-ltst-wgt-hdr
+    const innings2_batting = [];
+    const innings2_fall_of_wickets = [];
+    const innings2_wickets = [];
+
+    $(innings2).children().each(function (e, ele) {
+        if (e === 0) {
+            const play = $(this).find('div.cb-scrd-itms');
+            $(play).each(function (i, ele) {
+                const playerName = $(this).find("div.cb-col.cb-col-25 > a").text().trim();
+                const playerNameHref = $(this).find("div.cb-col.cb-col-25 > a").attr('href') || '/profiles/1427578/zakir-patel';
+
+                const PlayerIdPart = playerNameHref.split("profiles/")[1];
+                const Player_id = PlayerIdPart.split("/")[0];
+
+                const PlayerWickets = $(this).find("div.cb-col.cb-col-33>span").text().trim();
+                const runs = $(this).find('div.cb-col.cb-col-8.text-right').eq(0).text().trim();
+                const bowl = $(this).find('div.cb-col.cb-col-8.text-right').eq(1).text().trim();
+                const four = $(this).find('div.cb-col.cb-col-8.text-right').eq(2).text().trim();
+                const six = $(this).find('div.cb-col.cb-col-8.text-right').eq(3).text().trim();
+                const strike_rate = $(this).find('div.cb-col.cb-col-8.text-right').eq(4).text().trim();
+
+                // console.log(PlayerWickets);
+
+                if (playerName) {
+                    innings2_batting.push({
+                        playerName,
+                        Player_id,
+                        PlayerWickets,
+                        runs,
+                        bowl,
+                        four,
+                        six,
+                        strike_rate
+                    })
+                }
+
+            });
+        } else if (e === 2) {
+            const fall_of_wickets = $(this);
+            $(fall_of_wickets).children().each(function () {
+                const player = $(this).find("span > a").text();
+
+                const playerNameHref = $(this).find("span > a").attr('href') || '/profiles/1427578/zakir-patel';
+                const PlayerIdPart = playerNameHref.split("profiles/")[1];
+                const player_id = PlayerIdPart.split("/")[0];
+
+                const runs = $(this).text().split("(")[0].trim();
+                const bowl = $(this).text().split(",")[1].split(")")[0].trim();
+                innings2_fall_of_wickets.push({ player, player_id, runs, bowl })
+            })
+        } else if (e === 3) {
+            const Bowler = $(this);
+            $(Bowler).children().each(function () {
+                const BowlerItem = $(this);
+                $(BowlerItem).each(function () {
+                    const Name = $(this).find("a.cb-text-link").text();
+
+                    const playerNameHref = $(this).find("a.cb-text-link").attr('href') || '/profiles/1427578/zakir-patel';
+                    const PlayerIdPart = playerNameHref.split("profiles/")[1];
+                    const PlayerId = PlayerIdPart.split("/")[0];
+
+                    const Over = $(this).find("div.cb-col.cb-col-8.text-right").eq(0).text();
+                    const Maden = $(this).find("div.cb-col.cb-col-8.text-right").eq(1).text();
+                    const Run = $(this).find("div.cb-col.cb-col-10.text-right").eq(0).text();
+                    const Wickets = $(this).find("div.cb-col.cb-col-8.text-right").eq(2).text();
+                    const NoBowl = $(this).find("div.cb-col.cb-col-8.text-right").eq(3).text();
+                    const Wide = $(this).find("div.cb-col.cb-col-8.text-right").eq(4).text();
+                    const Eco = $(this).find("div.cb-col.cb-col-10.text-right").eq(1).text();
+
+                    if (Name) {
+                        innings2_wickets.push({
+                            Name,
+                            PlayerId,
+                            Over,
+                            Maden,
+                            Run,
+                            Wickets,
+                            NoBowl,
+                            Wide,
+                            Eco
+                        })
+                    }
+                })
+            })
         }
     });
 
@@ -103,10 +227,20 @@ const scorecards = async (req, res) => {
             seriesName,
             series_id
         },
-        innings_1: {
-            innings1_batting,
-            innings1_fall_of_wickets
-        }
+        innings: [
+            {
+                innings_1: {
+                    batting: innings1_batting,
+                    fall_of_wickets: innings1_fall_of_wickets,
+                    wickets: innings1_wickets
+                },
+                innings_2: {
+                    batting: innings2_batting,
+                    fall_of_wickets: innings2_fall_of_wickets,
+                    wickets: innings2_wickets
+                }
+            }
+        ]
     });
 };
 
